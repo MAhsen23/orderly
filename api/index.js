50464d9255cfeccb1fab297be398397df99e859e
AHSEN <ahsenikhan@gmail.com>
2024-12-02 03:08:01 +0500

    Add node modules in gitignore

--- api/index.js
+++ api/index.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const routes = require('./src/routes/routes');
-const PORT = process.env.PORT || 3002;
+const routes = require('../src/routes/routes');
 
 const app = express();
 app.use(express.json());
@@ -12,6 +11,4 @@ app.use(cors());
 mongoose.connect(process.env.MONGODB_URI, {}).then(() => console.log('MongoDB Connected')).catch((err) => console.error('Could not connect to MongoDB', err));
 
 app.use('/api', routes);
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
