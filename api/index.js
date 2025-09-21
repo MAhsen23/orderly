755b90cbf9ef2e4c3cd06f12e88511fee2ee4f6f
Muhammad Ahsen <ahsen.shiekh23@gmail.com>
2025-09-21 15:12:12 +0500

    changes in db and index

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI, {}).then(() => console.log('MongoDB Connected')).catch((err) => console.error('Could not connect to MongoDB', err));
+//mongoose.connect(process.env.MONGODB_URI, {}).then(() => console.log('MongoDB Connected')).catch((err) => console.error('Could not connect to MongoDB', err));
 
 app.use('/api', routes);
 app.get('/', (req, res) => {
