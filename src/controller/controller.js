1a249c11fd5e00a1ed33ec58428fd9308ef583e6
Muhammad Ahsen <ahsen.shiekh23@gmail.com>
2025-09-21 14:42:20 +0500

    Changes in vercel.json

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -6,7 +6,7 @@ import { sendOTPEmail, generateOTP } from '../utils/otpUtils.js';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config()
-const genAI = new GoogleGenerativeAI("AIzaSyBdsQszJiV8fD_k9MGogtmPDmFtxRvgBTA");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export const createUser = async (req, res) => {
     try {
